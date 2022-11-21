8a355c30eb2eccbcd68cbfc3358fda70abfbb95a
Shobhit Nautiyal <88445550+shobhit1812@users.noreply.github.com>
2022-11-21 08:32:52 +0530

    Update appApi.js

--- frontend/src/services/appApi.js
+++ frontend/src/services/appApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 const appApi = createApi({
   reducerPath: "appApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://sanvaad-backend.onrender.com/",
+    baseUrl: "http://localhost:5001",
   }),
   endpoints: (builder) => ({
     // creating the user
